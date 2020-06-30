b122c455873edce84ab4039928a7ebe67c75dff5
Dalton <daltonbassett@gmail.com>
2020-06-30 18:49:18 -0400

    prepping for global data

--- driver.py
+++ driver.py
@@ -1,19 +1,29 @@
 import time
 import datetime
 import bs4
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def update():
+    _now = datetime.datetime.now()
+    now_index = str(_now.month) + '/' + str(_now.day) + '/' + str(_now.year) + ' ' + str(_now.hour - 4) + ':' + str(
+        _now.minute) + ' EDT'
+    exec(open('covid19.py').read())
+    with open('index.html') as file:
+        txt = file.read()
+        soup = bs4.BeautifulSoup(txt, features='html5lib')
+    for p_tag in soup.find_all('p'):
+        if 'Last Updated:' in p_tag.contents[0]:
+            p_tag.string = 'Last Updated: ' + now_index
+            with open('index.html', 'w') as f:
+                f.write(str(soup))
+
+
+update()
 
 while True:
     now = datetime.datetime.now()
-    now_index = str(now.month) + '/' + str(now.day) + '/' + str(now.year) + ' ' + str(now.hour) + ':' + str(now.minute) + 'AM EDT'
-    if now.hour == 6:
-        exec(open('covid19.py').read())
-        with open('index.html') as file:
-            txt = file.read()
-            soup = bs4.BeautifulSoup(txt, features='lxml')
-        for p_tag in soup.find_all('p'):
-            if 'Last Updated:' in p_tag.contents[0]:
-                p_tag.string = 'Last Updated: ' + now_index
-                hi = 0
-                with open('index.html', 'w') as f:
-                    f.write(str(soup))
-    time.sleep(3550)
+    if now.hour == 10:
+        update()
+    time.sleep(3595)
