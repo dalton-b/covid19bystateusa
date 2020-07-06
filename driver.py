691690c9e74d13c9af60cf81e8db89019bb63d25
Dalton <daltonbassett@gmail.com>
2020-07-06 12:02:00 -0400

    Adjusted size of display boxes in Global page

--- driver.py
+++ driver.py
@@ -11,19 +11,24 @@ import datetime
 import math
 
 
-def update():
-    _now = datetime.datetime.now()
-    now_index = str(_now.month) + '/' + str(_now.day) + '/' + str(_now.year) + ' ' + str(_now.hour - 4) + ':' + str(
-        _now.minute) + ' EDT'
-    exec(open('covid19.py').read())
-    with open('index.html') as file:
+def update_time(filename):
+    now = datetime.datetime.now()
+    now_index = str(now.month) + '/' + str(now.day) + '/' + str(now.year) + ' ' + str(now.hour - 4) + ':' + str(
+        now.minute) + ' EDT'
+    with open(filename) as file:
         txt = file.read()
         soup = bs4.BeautifulSoup(txt, features='html5lib')
     for p_tag in soup.find_all('p'):
         if 'Last Updated:' in p_tag.contents[0]:
             p_tag.string = 'Last Updated: ' + now_index
-            with open('index.html', 'w') as f:
+            with open(filename, 'w') as f:
                 f.write(str(soup))
+
+
+def update():
+    exec(open('covid19.py').read())
+    update_time('index.html')
+    update_time('global.html')
 
 
 update()
