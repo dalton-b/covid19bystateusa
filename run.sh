10942a810ef900f670f4e4d9b54d8366a0371f68
dalton-b <daltonbassett@gmail.com>
2020-11-07 08:59:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# git clone https://github.com/dalton-b/covid19bystateusa.git
-# cd covid19bystateusa
-# python3 -m venv ./env
-# source ./env/bin/activate
-# pip3 install -e .
-# covid_update
-# # cd covid19bystateusa
-# git add .
-# git commit -m "test_01"
-# git remote set-url origin git@github.com:dalton-b/covid19bystateusa.git
-# GIT_SSH_COMMAND='ssh -i' ${{secrets.SECRET_KEY}} git push
 echo "Starting run.sh..."
 echo "Creating virtual environment..."
 python3 -m venv ./env
@@ -28,5 +17,5 @@ git commit -m "test_01"
 echo "Setting origin..."
 git remote set-url origin git@github.com:dalton-b/covid19bystateusa.git
 echo "Pushing changes..."
-GIT_SSH_COMMAND='ssh -i' ${{secrets.SECRET_KEY}} git push
+GIT_SSH_COMMAND='ssh -i' '$1 git push
 echo "Done!"
