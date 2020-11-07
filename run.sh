f2a31edfeeba764ee0eced7869f722c41afe657a
dalton-b <daltonbassett@gmail.com>
2020-11-06 22:10:06 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-git clone https://github.com/dalton-b/covid19bystateusa.git
-cd covid19bystateusa
+# git clone https://github.com/dalton-b/covid19bystateusa.git
+# cd covid19bystateusa
 python3 -m venv ./env
 source ./env/bin/activate
 pip3 install -e .
 covid_update
-cd covid19bystateusa
+# cd covid19bystateusa
 git add .
 git commit -m "test_01"
 git remote set-url origin git@github.com:dalton-b/covid19bystateusa.git
