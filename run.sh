700221debf1eebe0793185d7ee96f4a8d22a5826
dalton-b <daltonbassett@gmail.com>
2020-11-06 22:40:23 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,10 @@
 # git commit -m "test_01"
 # git remote set-url origin git@github.com:dalton-b/covid19bystateusa.git
 # GIT_SSH_COMMAND='ssh -i' ${{secrets.SECRET_KEY}} git push
-ls
+echo "Starting run.sh..."
+echo "Creating virtual environment..."
 python3 -m venv ./env
+echo "Activating environment..."
 source ./env/bin/activate
+echo "Installing packages..."
 pip3 install -e .
