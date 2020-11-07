c19fae14cd44973d7c74f3ef6fe75fba9c945118
dalton-b <daltonbassett@gmail.com>
2020-11-06 19:59:50 -0500

    Another attempt at auto runs - every 3 minutes

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git clone https://github.com/dalton-b/covid19bystateusa.git
+cd covid19bystateusa
+python3 -m venv ./env
+source ./env/bin/activate
+pip3 install -e .
+covid_update
+cd covid19bystateusa
+git add .
+git commit -m "test_01"
+git remote set-url origin git@github.com:dalton-b/covid19bystateusa.git
+GIT_SSH_COMMAND='ssh -i ${{secrets.SECRET_KEY}}' git push
