23b1e03bad7787555f581bf8d7f786849ca385b0
dalton-b <daltonbassett@gmail.com>
2020-11-07 08:59:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,5 +17,5 @@ git commit -m "test_01"
 echo "Setting origin..."
 git remote set-url origin git@github.com:dalton-b/covid19bystateusa.git
 echo "Pushing changes..."
-GIT_SSH_COMMAND='ssh -i' '$1 git push
+GIT_SSH_COMMAND='ssh -i' '$1' git push
 echo "Done!"
